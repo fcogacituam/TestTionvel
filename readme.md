46e25788fd2355ed4743f3ab19e69d3a601e79a5
fcogacituam <fcogacituam@gmail.com>
2018-02-12 22:32:40 -0300

    readme and Test

--- readme.md
+++ readme.md
@@ -1,2 +1,38 @@
-##
-Laravel
+Test para Tionvel
+Francisco Gacitúa Maturana
+
+
+<h1>Instalación</h1>
+Descargue o clone el repositorio desde https://github.com/fcogacituam/TestTionvel.git
+si descargó el archivo .zip, descomprimir.
+abrir el CMD y dirigirse al directorio del proyecto
+escribir el comando "composer install"
+una vez instaladas las dependencias inicie el servidor con el comando "php artisan serve"
+abra su navegador y escriba la dirección http://127.0.0.1:8000
+Ya está listo para utilizar y probar!
+
+<h1>Prueba unitaria</h1>
+Para poder realizar la prueba unitaria, deje corriendo el servidor anterior y abra una nueva ventana de CMD, diríjase al diectorio del proyecto
+ingrese el comando "php artisan dusk"
+Si todo salió bien, la consola debiera arrojar el mensaje "OK (1 test, 4 assertions)"
+
+La prueba unitaria se encuentra (comentada) en el directorio tests/Browser/ExampleTest.php
+
+
+
+<h1>Installation</h1>
+Download or clone this repo from https://github.com/fcogacituam/TestTionvel.git
+if you download it, uncompress.
+Open your CMD and go to the project's main directory
+write the command "composer install"
+When all dependencies are installed, start the server typing the command "php artisan serve"
+open your browser and write the URL http://127.0.0.1:8000
+You are ready to test and use!
+
+<h1>Unit Test</h1>
+In order to make the Unit Test, let the server running and open another CMD window
+go to the project's main directory.
+write the command "php artisan dusk"
+if everything is Ok, the console must show the message: "OK (1 test, 4 assertions)"
+
+The unit Test is found (commented) in "tests/Browser/ExampleTest.php"
--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -17,19 +17,32 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
+                    // We use script to type dates
                      ->script(["document.querySelector('#fecha1').value = '2017-10-13' ",]);
+                     // the first date is October 10, 2017. The in mail example date
             $browser->script(["document.querySelector('#fecha2').value = '2018-03-27' ",]);
             $browser->script(["document.querySelector('#fecha3').value = '2018-04-08' ",]);
             $browser->script(["document.querySelector('#fecha4').value = '2018-05-14' ",]);
             $browser->type('fecha1n','8')
+                    // we add 8 business days to the example date
                     ->type('fecha2n','5')
                     ->type('fecha3n','2')
                     ->type('fecha4n','4')
                     ->press("Enviar")
+                    //we send the values by pressing the "Enviar" button
                     ->assertSee("2017-10-23")
+                    //if no errors found, we spect to see the date 2017-10-23
                     ->assertSee("2018-04-04")
                     ->assertSee("2018-04-10")
                     ->assertSee("2018-05-18");
         });
+
+        //The next 3 dates are to test the correct functioning of the software
+        //We add 5 days to 2018-03-27.. considering that March 30, and March 31 are holiday, and April 1st is Sunday, the correct date must be 2018-04-04
+        //We add 2 days to 2018-04-08. the resulting date must be 2018-04-10, because there are not holidays neither sundays between these two dates, also, we start in Sunday for testing
+        // we add 4 days to 2018-05-14. We start on Monday, there are not holidays, so, te resulting date must be 2018-05-18.
+
+        // All dates are in order, that is, second date is greater than first date, third date is greater than second date, and fourth date is greather than third date. if we change some date, to break this order, when we run the command "php artisan dusk" we'll receive an error.
     }
+
 }
